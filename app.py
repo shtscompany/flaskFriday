abe07e9ab76100cfe9af4a95278e495dec16c602
shtscompany <shtscompany@gmail.com>
2024-11-13 03:51:53 +0400

    this is bootstrap link change project

--- app.py
+++ app.py
@@ -3,14 +3,6 @@ from flask import Flask, render_template, redirect
 app=Flask(__name__)
 app.debug=True
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-# create Custom Error Pages
-
-# Invalid URL
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
@@ -18,3 +10,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+@app.route('/')
+def index():
+    first_name="John Tsiklauri"
+    stuff="this is the <strong>bold<strong> tag"
+    favorite_pizza=["Pepparony", "cheese", "Mushrooms", 41]
+    return render_template("index.html", 
+                           first_name=first_name,
+                           stuff=stuff,
+                           favorite_pizza=favorite_pizza)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", name=name)

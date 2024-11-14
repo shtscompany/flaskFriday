087c94a22b631e8aa9f9d5d41d5c9af3b87749b5
shtscompany <shtscompany@gmail.com>
2024-11-14 14:22:52 +0400

    flash messages

---
+++ static/js/404.js
@@ -0,0 +1,12 @@
+const errorMessage="404 Error Page"
+const errorMessage4="4"
+const errorMessage0="0"
+const pageNotFound="Page Not Found"
+
+document.getElementById("errorMessage").innerHTML=errorMessage
+
+document.getElementsByClassName("four").innerHTML=errorMessage4
+document.getElementsByClassName("zero").innerHTML=errorMessage0
+
+document.getElementById("pageNotFound").innerHTML=pageNotFound
+
